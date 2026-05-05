d7841f9cc0027889eb611455b9ba96dc36898b6d
Alumno1 <Alumno1@PROGRESA36>
2026-05-05 13:36:22 +0200

    Ejercicio 1

--- simulacionED3Trimestre/Program.cs
+++ simulacionED3Trimestre/Program.cs
@@ -40,25 +40,4 @@ class Libro
 	{
 		return this.disponible;
 	}
-
-	public static void Main(string[] args)
-	{
-		List<Libro> libros = new List<Libro>();
-
-		libros.Add(new Libro("El Quijote", "Miguel de Cervantes", 1605));
-		libros.Add(new Libro("Cien años de soledad", "Gabriel García Márquez", 1967));
-		libros.Add(new Libro("Don Juan Tenorio", "José Zorrilla", 1844));
-
-		for (int i = 0; i < libros.Count; i++)
-		{
-			Console.WriteLine(libros[i].ToString());
-			if (libros[i].getAutor().Contains("Orwell"))
-			{
-				Console.WriteLine(libros[i].ToString());
-			}
-		}
-
-		Console.WriteLine(DateTime.Now);
-		Console.WriteLine(DateTime.Now.ToShortDateString());
-	}
 }
